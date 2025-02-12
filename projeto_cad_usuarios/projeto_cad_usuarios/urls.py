6cd107c28e530707f9008ec1acf5e8eb497b9873
BrunoBarrosR <bruno_barros@hotmail.com.br>
2025-02-12 18:26:13 -0300

    HTML, models, ulrs, views

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.urls import path
+from app_cad_usuarios import views
 
 urlpatterns = [
-    #
-
+    # rota, view responsavel, nome de referencia
+    # cadastro.com
+    path('',views.home, name='home'),
+    # cadastro.com/usuarios
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
